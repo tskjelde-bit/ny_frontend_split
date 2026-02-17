40079f2e2c1cb31653f11db489ebb672f9bc8d10
tskjelde-bit <t.skjelde@gmail.com>
2026-02-18 00:59:46 +0100

    Configure deployment for ny_frontend_split

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
-      base: mode === 'production' ? '/ny_frontend_typo/' : '/',
+      base: mode === 'production' ? '/ny_frontend_split/' : '/',
       server: {
         port: 3000,
         host: '0.0.0.0',
